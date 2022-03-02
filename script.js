11cf607cbd16498d99151cbeffd7d44a54ba4432
CKline22 <CKline.1022@gmail.com>
2022-03-02 14:11:28 -0500

    trivia-game

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-const textElement = document.getElementById('text')
-const optionButtons = document.getElementById('option-buttons')
+const startButton = document.getElementById('start-button')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer() {
+
+}
+
+const question
+//14:50 to resume
